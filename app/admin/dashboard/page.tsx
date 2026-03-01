ed7bda25e3165ab01d0a0a3ee0447c379de64e54
UnLogic <johannocuaman@gmail.com>
2026-02-28 23:27:21 -0800

    Admin Dashboard
    Added information displays to admin dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,18 +1,45 @@
 "use client";
 
-export default function AdminDashboard() {
-  return (
-    <main className="min-h-screen">
-      {/* Top section */}
-      <div className="mb-12">
-        {/* Top-left welcome */}
-        <h2 className="font-bold">
-          <span className="text-white text-4xl">Welcome, </span>
-          <span className="text-slate-400 text-2xl">User</span>
-        </h2>
-      </div>
+import {
+  DollarSign,
+  Users,
+  UserCheck,
+  UserPlus,
+  TrendingDown,
+} from "lucide-react";
+import KpiCard from "../components/KPICard";
+import RevenueChart from "../components/RevenueChart";
+import RecentActivity from "../components/RecentActivity";
 
-      {/* Dashboard content */}
-    </main>
-  );
+export default function AdminDashboard() {
+    return (
+      <main className="min-h-screen">
+          {/* Top section */}
+          <div className="mb-12">
+            <h2 className="font-bold">
+              <span className="text-white text-4xl">Welcome, </span>
+              <span className="text-slate-400 text-2xl">User</span>
+            </h2>
+          </div>
+
+          {/* KPI Row */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
+            <KpiCard icon={DollarSign} title="MRR" value="$0" />
+            <KpiCard icon={Users} title="Active Subscriptions" value="0" />
+            <KpiCard icon={UserCheck} title="Active Users (7 Days)" value="0" />
+            <KpiCard icon={UserPlus} title="New Signups (30 Days)" value="0" />
+            <KpiCard icon={TrendingDown} title="Churn" value="0%" />
+          </div>
+
+          {/*Line Chart */}
+          <div>
+            <RevenueChart/>
+          </div>
+
+          {/* Recent Activity */}
+          <div>
+            <RecentActivity/>
+          </div>
+      </main>
+    );
 }
