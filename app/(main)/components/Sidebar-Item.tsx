7849571995452fd9577f506b9df216d000991ec7
UnLogic <johannocuaman@gmail.com>
2026-04-24 12:46:07 -0700

    Stopped Sidebar-Item complaint

--- app/(main)/components/Sidebar-Item.tsx
+++ app/(main)/components/Sidebar-Item.tsx
@@ -19,7 +19,7 @@ export const SidebarItem = ({ label, icon: Icon, href }: Props) => {
 
   return (
     <Button
-      variant={active ? "sidebarDarkActive" : "sidebarDark"}
+      variant={active ? "nav" : "navActive"}
       className="justify-start text-white font-extrabold text-1xl h-13 gap-3"
       asChild
     >
